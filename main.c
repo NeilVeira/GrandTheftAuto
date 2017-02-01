e631c8f794e22c44049ef850265e7558541019f9
yaronm <yaronm93@gmail.com>
2017-02-01 15:29:45 -0500

    updated main

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include "xil_io.h"
 #include "myip.h"
 
-volatile unsigned int *myip_ptr = (unsigned int*) XPAR_MYIP_0_S00_AXI_BASEADDR;
+volatile unsigned int *myip_ptr = (unsigned int*) XPAR_Fib_LFSR_0_S00_AXI_BASEADDR;
 
 int main()
 {
+	Fib_LFSR_mWriteReg(XPAR_Fib_LFSR_0_S00_AXI_BASEADDR, 0, 0x00000004);
+	Fib_LFSR_mWriteReg(XPAR_Fib_LFSR_0_S00_AXI_BASEADDR, 0x10, 0x00000001);
+	Fib_LFSR_mWriteReg(XPAR_Fib_LFSR_0_S00_AXI_BASEADDR, 0, 0x00000003);
+	
 	// Low Level Driver
-	MYIP_mWriteReg(XPAR_MYIP_0_S00_AXI_BASEADDR, 0, 0xDEADBEEF);
-
-	// Direct Access
-	*(myip_ptr + 1) = 0xBAADF00D;
+	for (int i =0; i<=100; i++)
+		for (int j=0; j<=1000000;j++)
+			Fib_LFSR_mReadReg(XPAR_MYIP_0_S00_AXI_BASEADDR, 0xC);
 
 	return 0;
 }
