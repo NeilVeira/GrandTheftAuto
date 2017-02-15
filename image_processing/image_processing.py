029ba1d5775f0909fb3478f24de9fd3e69e5795e
NeilVeira <neil.veira@mail.utoronto.ca>
2017-02-15 12:33:18 -0500

    Slightly simpler computation algorithm

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -9,17 +9,16 @@ def find_red(pixels):
     h = len(pixels)
     w = len(pixels[0])
     output = []
-    target = [221,221,221]
+    target = [180,45,60]
     
     for y in xrange(h):
         row = []
         for x in xrange(w):
             r,g,b = pixels[y][x]
-            #recognize red, pink, or orange
-            #err = (r-target[0])**2 + (g-target[1])**2 + (b-target[2])**2
-            #if err <= 3500:   
-            if (r >= 50 and r > 2.5*g and r > 1.5*b) or (r >= 150 and r > 2.0*g and r > 1.5*b):        
-                #print err
+            #recognize red, pink, or orange       
+            #if (r >= 50 and r > 2.5*g and r > 1.5*b) or (r >= 150 and r > 2.0*g and r > 1.5*b): 
+            err = (r-target[0])**2 + (g-target[1])**2 + (b-target[2])**2
+            if err <= 3500:  
                 row.append(1)
             else:
                 row.append(0)
@@ -37,44 +36,34 @@ def detect_edges(pix):
     high = HIGH*(2*k+1)*(2*k+1)
     output = [list(row) for row in pix]
     
-    #column_sum[y][x] = sum of pixels along column x from y-2*k to y
-    #precompute all entries
-    column_sum = [[0]*w for _ in range(h)]   
-    #seperately compute the first entry for y = 2*k
+    #colsum[y][x] = sum of pixels along column x from y-2*k to y
+    #precompute all entries 
+    colsum = [[0]*w for _ in range(h)] 
+    #compute seperately for y = 0
     for x in xrange(w):
-        s = 0
-        for y in xrange(2*k+1):
-            s += pix[y][x]
-        column_sum[2*k][x] = s
-    #compute other y using update relation
-    for y in xrange(2*k+1,h):
+        colsum[0][x] = pix[0][x]
+    for y in xrange(1,h):
         for x in xrange(w):
-            column_sum[y][x] = column_sum[y-1][x] - pix[y-(2*k+1)][x] + pix[y][x]
-            assert 0 <= column_sum[y][x] <= 2*k+1
-    #for row in column_sum[160:180]:
-    #    print row[270:290]
+            colsum[y][x] = colsum[y-1][x] + pix[y][x]
+            if y-(2*k+1) >= 0:
+                colsum[y][x] -= pix[y-(2*k+1)][x]
     
-    #for each pixel, sum all pixels in 2k x 2k window around it.
-    for y in xrange(k,h-k):
-        #do first column seperately
-        prev = sum(column_sum[y+k][0:2*k+1])
-        if low <= prev <= high:
-            output[y][k] = 2
-        
-        for x in xrange(k+1,w-k):
-            #s_correct = sum(column_sum[y+k][x-k:x+k+1])
-            s = prev - column_sum[y+k][x-k-1] + column_sum[y+k][x+k] 
-            if low <= s <= high:
-                output[y][x] = 2
+    #for each pixel, sum all pixels in (2k+1) x (2k+1) window around it.
+    for y in xrange(k,h-k):   
+        prev = colsum[y+k][0]
+        for x in xrange(1,w-k):
+            s = prev + colsum[y+k][x]
+            if x-2*k-1 >= 0:
+                s -= colsum[y+k][x-2*k-1]
+            if x-k >= 0 and low <= s <= high:
+                output[y][x-k] = 2
             prev = s
-                                
+                     
     return output
 
 
-image = "16.jpg"
-
 print "Getting input pixels"
-im = Image.open("test_images/"+image)
+im = Image.open("test_images/16.jpg")
 #get pixels
 w = im.size[0]
 h = im.size[1]
@@ -84,20 +73,42 @@ for y in xrange(h):
         pixels[y][x] = im.getpixel((x,y))
 #print pixels
 
-output = find_red(pixels)
-output = detect_edges(output)
+colour = find_red(pixels)
+output = detect_edges(colour)
 
 #create a new image with just red on black background and green edges
 print "Setting output image"
 im = Image.new("RGB", (w,h), "black")
-pixels = im.load()
+out_pixels = im.load()
 for y in xrange(h):
     for x in xrange(w):
         if output[y][x] == 0:
-            pixels[x,y] = (0,0,0)
+            out_pixels[x,y] = (0,0,0)
         elif output[y][x] == 1:
-            pixels[x,y] = (255,0,0) 
+            out_pixels[x,y] = (255,0,0) 
         elif output[y][x] == 2:
-            pixels[x,y] = (0,255,0)
+            out_pixels[x,y] = (0,255,0)
 im.show()
-im.save("edges/"+image)
+im.save("tmp.jpg")
+
+
+'''#write testbench inputs, expected colour, and edge outputs to files
+decoded = open("image_processor/image_processor.srcs/sim_1/imports/image_processing/tb_input.txt","w")
+for y in xrange(h):
+    for x in xrange(w):
+        r,g,b = pixels[y][x]
+        decoded.write("%i %i %i %i %i\n" %(x,y,r,g,b))
+decoded.close()
+
+colour_file = open("image_processor/image_processor.srcs/sim_1/imports/image_processing/tb_colour.txt","w")
+for y in xrange(h):
+    for x in xrange(w):
+        colour_file.write("%i\n" %(colour[y][x]))
+colour_file.close()
+
+edge_file = open("image_processor/image_processor.srcs/sim_1/imports/image_processing/tb_edges.txt","w")
+for y in xrange(h):
+    for x in xrange(w):
+        if output[y][x] == 2:
+            edge_file.write("%i %i\n" %(x,y))
+edge_file.close()'''
